64c611603bf71356a984af5d6661e2a2b4c87a1d
MrFatra <ramadhanfatra2004@gmail.com>
2024-07-07 08:31:33 +0700

    feat(detail): add navbar

--- lib/screens/destination_detail.dart
+++ lib/screens/destination_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class DestinationDetail extends StatelessWidget {
   const DestinationDetail({super.key, required this.images});
@@ -6,10 +7,81 @@ class DestinationDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder(
-      child: ListView.builder(
-        itemCount: images.length,
-        itemBuilder: (context, index) => Image.network(images[index]),
+    return Scaffold(
+      appBar: AppBar(
+          scrolledUnderElevation: 25,
+          toolbarHeight: 80,
+          backgroundColor: Colors.white,
+          leading: SizedBox(),
+          leadingWidth: 25,
+          title: Text('Destinasi Pariwisata'),
+          actions: [
+            ResponsiveVisibility(
+              visible: false,
+              visibleConditions: [Condition.largerThan(name: MOBILE)],
+              child: Container(
+                margin: EdgeInsets.only(right: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text('Home'),
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text('About'),
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text('Contact'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ResponsiveVisibility(
+              visible: false,
+              visibleConditions: [Condition.smallerThan(name: TABLET)],
+              child: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ListTile(
+                            leading: Icon(Icons.home),
+                            title: Text('Home'),
+                            onTap: () {},
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.info),
+                            title: Text('About'),
+                            onTap: () {},
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.contact_mail),
+                            title: Text('Contact'),
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      body: Placeholder(
+        child: ListView.builder(
+          itemCount: images.length,
+          itemBuilder: (context, index) => Image.network(images[index]),
+        ),
       ),
     );
   }
