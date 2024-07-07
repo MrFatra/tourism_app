0e07c979a1e870f5a471b02e252fc7d18a73703f
MrFatra <ramadhanfatra2004@gmail.com>
2024-07-07 08:31:58 +0700

    fix: responsive & animation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'package:tourism_app/cubit/home_cubit.dart';
+import 'package:tourism_app/helper/constant.dart';
 import 'package:tourism_app/repositories/destination.dart';
 import 'destination_detail.dart';
 
@@ -27,8 +28,73 @@ class _HomeState extends State<Home> {
       ],
       child: Scaffold(
         appBar: AppBar(
+          scrolledUnderElevation: 25,
+          toolbarHeight: 80,
+          backgroundColor: Colors.white,
+          leading: SizedBox(),
+          leadingWidth: 25,
           title: Text('Destinasi Pariwisata'),
-          centerTitle: true,
+          actions: [
+            ResponsiveVisibility(
+              visible: false,
+              visibleConditions: [Condition.largerThan(name: MOBILE)],
+              child: Container(
+                margin: EdgeInsets.only(right: 50),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text('Home'),
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text('About'),
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: Text('Contact'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ResponsiveVisibility(
+              visible: false,
+              visibleConditions: [Condition.smallerThan(name: TABLET)],
+              child: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) => Container(
+                      color: Colors.white,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          ListTile(
+                            leading: Icon(Icons.home),
+                            title: Text('Home'),
+                            onTap: () {},
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.info),
+                            title: Text('About'),
+                            onTap: () {},
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.contact_mail),
+                            title: Text('Contact'),
+                            onTap: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
         body: BlocBuilder<HomeCubit, HomeState>(
           builder: (context, state) {
@@ -40,23 +106,23 @@ class _HomeState extends State<Home> {
               return GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: ResponsiveValue<int>(
-                      context,
-                      defaultValue: 1,
-                      conditionalValues: [
-                        Condition.smallerThan(
-                          name: TABLET,
-                          value: 1,
-                        ),
-                        Condition.equals(
-                          name: TABLET,
-                          value: 2,
-                        ),
-                        Condition.largerThan(
-                          name: TABLET,
-                          value: 3,
-                        ),
-                      ],
-                    ).value,
+                    context,
+                    defaultValue: 1,
+                    conditionalValues: [
+                      Condition.smallerThan(
+                        name: TABLET,
+                        value: 1,
+                      ),
+                      Condition.equals(
+                        name: TABLET,
+                        value: 2,
+                      ),
+                      Condition.largerThan(
+                        name: TABLET,
+                        value: 3,
+                      ),
+                    ],
+                  ).value,
                   childAspectRatio: 1,
                   crossAxisSpacing: 10,
                   mainAxisSpacing: 10,
@@ -92,66 +158,83 @@ class _HomeState extends State<Home> {
                             borderRadius: BorderRadius.circular(15.0),
                           ),
                           elevation: 5,
-                          child: Stack(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Column(
-                                mainAxisSize: MainAxisSize.min,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(15.0),
-                                      topRight: Radius.circular(15.0),
-                                    ),
-                                    child: Image.network(
-                                      state.destinations[index].images[0],
-                                      height: ResponsiveBreakpoints.of(context).smallerOrEqualTo(TABLET) ? 100 : 150,
-                                      width: double.infinity,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      state.destinations[index].name,
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
+                              Flexible(
+                                child: Stack(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(15.0),
+                                        topRight: Radius.circular(15.0),
+                                      ),
+                                      child: Image.network(
+                                        state.destinations[index].images[0],
+                                        width: double.infinity,
+                                        fit: BoxFit.cover,
                                       ),
                                     ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 8.0),
-                                    child: Text(
-                                      state.destinations[index].description,
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.grey[600],
+                                    Positioned(
+                                      bottom: 0,
+                                      right: 0,
+                                      child: Container(
+                                        padding: EdgeInsets.all(8),
+                                        decoration: BoxDecoration(
+                                            color: kPrimaryColor,
+                                            borderRadius: BorderRadius.only(
+                                                topLeft: Radius.circular(10))),
+                                        child: Row(
+                                          children: [
+                                            Text(
+                                              'Lihat Selengkapnya',
+                                              style: TextStyle(
+                                                  color: Colors.white),
+                                            ),
+                                            Icon(Icons.arrow_forward,
+                                                color: Colors.white, size: 18)
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  ),
-                                ],
+                                    )
+                                        .animate(
+                                            target:
+                                                isHoveringList[index] ? 1 : 0)
+                                        .flipV(
+                                            duration: 200.ms,
+                                            alignment: Alignment(0.9, 0)),
+                                  ],
+                                ),
                               ),
-                              Positioned(
-                                bottom: 8,
-                                right: 8,
-                                child: TextButton(
-                                  onPressed: () => {},
-                                  child: Row(
-                                    children: [
-                                      Text('Lihat Selengkapnya'),
-                                      Icon(Icons.arrow_forward, size: 20,)
-                                    ],
-                                  ),
-                                )
-                                    .animate(
-                                        target: isHoveringList[index] == true
-                                            ? 1
-                                            : 0)
-                                    .fadeIn().moveY(begin: 10),
+                              Flexible(
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        state.destinations[index].name,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 8.0),
+                                      child: Text(
+                                        state.destinations[index].description,
+                                        maxLines: 2,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: TextStyle(
+                                          fontSize: 14,
+                                          color: Colors.grey[600],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ],
                           ),
